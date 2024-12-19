623d43a0096fdd22f0014c8f04f879d4abcc3ea7
Judalm Park (dami) <wnekfa1004@naver.com>
2024-12-20 00:41:13 +0900

    [feat] 포인트, 락 관련 커스텀 에러 메시지 추가

--- src/main/java/io/hhplus/tdd/CustomException.java
+++ src/main/java/io/hhplus/tdd/CustomException.java
@@ -14,7 +14,8 @@ public class CustomException extends RuntimeException {
         AMOUNT_EXCEPTION("AMOUNT가 음수일 수 없습니다.", HttpStatus.BAD_REQUEST),
         TOO_MANY_AMOUNT_EXCEPTION("포인트는 100만을 초과할 수 없습니다.", HttpStatus.BAD_REQUEST),
         USER_ID_EXCEPTION("유저 아이디는 음수일 수 없습니다.", HttpStatus.BAD_REQUEST),
-        NOT_ENOUGH_POINTS("보유 포인트가 부족합니다.", HttpStatus.BAD_REQUEST);
+        NOT_ENOUGH_POINTS_EXCEPTION("보유 포인트가 부족합니다.", HttpStatus.BAD_REQUEST),
+        WAIT_TIME_EXCEPTION("락 대기시간 초과", HttpStatus.LOCKED);
 
         private final String message;
         private final HttpStatus httpStatus;
