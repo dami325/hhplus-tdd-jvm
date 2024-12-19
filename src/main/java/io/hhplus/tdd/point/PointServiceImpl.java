8835db5f538e224d170de8ad586e5ab06361b6dc
Judalm Park (dami) <wnekfa1004@naver.com>
2024-12-20 00:44:53 +0900

    [feat] PointServiceImpl 동시성 처리 리팩터링

--- src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -8,6 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
 
 import static io.hhplus.tdd.CustomException.ErrorMessageCode;
 
@@ -18,65 +21,77 @@ public class PointServiceImpl implements PointService {
 
     private final PointHistoryTable pointHistoryTable;
     private final UserPointTable userPointTable;
+    private final ConcurrentHashMap<String, ReentrantLock> lockConcurrentHashMap = new ConcurrentHashMap();
+    private final String USER_LOCK_PREFIX = "USER_";
 
     @Override
     public UserPoint findUserPointById(long userId) {
-        userIdValidation(userId);
-        return userPointTable.selectById(userId);
+        String key = USER_LOCK_PREFIX + userId;
+        return withLock(key, () -> {
+            userIdValidation(userId);
+            return userPointTable.selectById(userId);
+        });
     }
 
     @Override
     public List<PointHistory> findPointHistoriesById(long userId) {
-        userIdValidation(userId);
-        return pointHistoryTable.selectAllByUserId(userId);
+        String key = USER_LOCK_PREFIX + userId;
+        return withLock(key, () -> {
+            userIdValidation(userId);
+            return pointHistoryTable.selectAllByUserId(userId);
+        });
     }
 
     @Override
     public UserPoint chargeUserPoint(long userId, long amount) {
+        String key = USER_LOCK_PREFIX + userId;
+        return withLock(key, () -> {
+            userIdValidation(userId);
+            amountValidation(amount);
 
-        userIdValidation(userId);
-        amountValidation(amount);
+            // 유저의 포인트 조회
+            UserPoint currentUserPoint = userPointTable.selectById(userId);
 
-        // 유저의 포인트 조회
-        UserPoint currentUserPoint = userPointTable.selectById(userId);
+            // 유저 포인트 합계
+            long totalAmount = currentUserPoint.point() + amount;
 
-        // 유저 포인트 합계
-        long totalAmount = currentUserPoint.point() + amount;
+            if (totalAmount > 1_000_000L) {
+                throw new CustomException(ErrorMessageCode.TOO_MANY_AMOUNT_EXCEPTION);
+            }
 
-        if(totalAmount > 1_000_000L){
-            throw new CustomException(ErrorMessageCode.TOO_MANY_AMOUNT_EXCEPTION);
-        }
+            // 히스토리에 충전 내역 저장
+            pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
 
-        // 히스토리에 충전 내역 저장
-        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
-
-        return userPointTable.insertOrUpdate(userId, totalAmount);
+            return userPointTable.insertOrUpdate(userId, totalAmount);
+        });
     }
 
 
     @Override
     public UserPoint useUserPoint(long userId, long amount) {
+        String key = USER_LOCK_PREFIX + userId;
+        return withLock(key, () -> {
+            userIdValidation(userId);
+            amountValidation(amount);
 
-        userIdValidation(userId);
-        amountValidation(amount);
+            // 유저의 포인트 조회
+            UserPoint currentUserPoint = userPointTable.selectById(userId);
 
-        // 유저의 포인트 조회
-        UserPoint currentUserPoint = userPointTable.selectById(userId);
+            long currentPoint = currentUserPoint.point();
 
-        long currentPoint = currentUserPoint.point();
+            // 현재 포인트가 입력받은 포인트 보다 적을 경우 사용할 수 없음
+            if (currentPoint < amount) {
+                throw new CustomException(ErrorMessageCode.NOT_ENOUGH_POINTS_EXCEPTION);
+            }
 
-        // 현재 포인트가 입력받은 포인트 보다 적을 경우 사용할 수 없음
-        if (currentPoint < amount) {
-            throw new CustomException(ErrorMessageCode.NOT_ENOUGH_POINTS);
-        }
+            // 히스토리에 차감 내역 저장
+            pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
 
-        // 히스토리에 차감 내역 저장
-        pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+            // 현재 포인트에서 입력받은 포인트 차감
+            long totalAmount = currentPoint - amount;
 
-        // 현재 포인트에서 입력받은 포인트 차감
-        long totalAmount = currentPoint - amount;
-
-        return userPointTable.insertOrUpdate(userId, totalAmount);
+            return userPointTable.insertOrUpdate(userId, totalAmount);
+        });
     }
 
     private void amountValidation(long amount) {
@@ -88,6 +103,19 @@ public class PointServiceImpl implements PointService {
     private void userIdValidation(long userId) {
         if (userId <= 0) {
             throw new CustomException(ErrorMessageCode.USER_ID_EXCEPTION);
+        }
+    }
+
+    /**
+     * 공통 락 처리 유틸리티 메서드
+     */
+    private <T> T withLock(String key, Supplier<T> action) {
+        ReentrantLock lock = lockConcurrentHashMap.computeIfAbsent(key, s -> new ReentrantLock(true));
+        try {
+            lock.lock();
+            return action.get();
+        } finally {
+            lock.unlock();
         }
     }
 }
